7e6af0ed9fa39fd559f930090b72ebb1e583cc65
Archis Gore <archis@concurix.com>
2017-11-17 20:22:17 -0800

    Execution simulator

--- internaljs/comparator.js
+++ internaljs/comparator.js
@@ -17,18 +17,97 @@ function attachComparators() {
   $("#gadget-find-btn").click(function(){
     scrollTablesToGadget(gadFindCont.val());
   });
+
+  $("#generate-rand-chain-btn").click(function(){
+    var chain = generateRandomChain(tablesToGadgets["table1"]);
+    $("#rop-chain-content").val(chain);
+  });
+
+  $("#exec-chain-btn").click(function(){
+    var chain = $("#rop-chain-content").val();
+    executeChain(chain);
+  });
+
 }
+
+function executeChain(chain) {
+  if (chain == "") {
+    return;
+  }
+
+  $("#execution").empty();
+
+  //How many rop tables are we working with?
+  var ropTableLen = tablesToGadgets.length;
+
+  //Let's set up our UI first
+  var th = $("<tr></tr>");
+  th.append("<td></td>");
+  for (name in tablesToGadgets) {
+    th.append("<td>Gadgets from " + name + "</td>");
+  }
+
+  var thead = $("<thead></thead>")
+  thead.append(th);
+  var table = $("<table/>");
+  table.append(thead);
+
+  $("#execution").append(table);
+
+  var tbody = $("<tbody/>");
+  table.append(tbody);
+
+  var addrs = chain.split(" ");
+  var brokenChains = {};
+
+  for (var i in addrs) {
+    var addr = addrs[i];
+    if (addr == "") continue;
+    
+    var tr = $("<tr/>");
+    tr.append("<td>" + addr + "</td>");
+
+    //Now go over all tables
+    for (name in tablesToGadgets) {
+      if (brokenChains[name]){
+        tr.append('<td class="broken">Chain broken</td>');
+        continue;
+      }
+
+      var gadgets = tablesToGadgets[name];
+      var index = findGadgetIndexForVaddr(gadgets, addr);
+      if (index < 0) {
+        brokenChains[name] = true;
+        tr.append('<td class="broken">No Gadget!</td>');
+      } else {
+        tr.append('<td class="intact">' + gadgets[index].gadget + '</td>');
+      }
+    }
+    tbody.append(tr);
+  }
+
+}
+
+function generateRandomChain(gadgets) {
+  var len = getRandomIntInclusive(5, 20);
+  var chain = ""
+  for (var i=0; i < len; i++) {
+    var nextgad = getRandomIntInclusive(0, gadgets.length-1);
+    chain += gadgets[nextgad].vaddr + " ";
+  }
+  return chain;
+}
+
 
 function scrollTablesToGadget(gadget) {
   $('.ropTable').each(function(idx, table){
     var jqt = $(table);
     var name = $(table).attr("id");
     var gadgets = tablesToGadgets[name];
-    for (var index in gadgets) {
-      if (gadgets[index].gadget == gadget) {
+    var index = findGadgetIndexForGadget(gadgets, gadget);
+    if (index >= 0) {
           scrollTableToSelectedRow(jqt, index);
           break;
-      }
     }
   });
 }
@@ -38,11 +117,9 @@ function scrollTablesToAddress(addr) {
     var jqt = $(table);
     var name = $(table).attr("id");
     var gadgets = tablesToGadgets[name];
-    for (var index in gadgets) {
-      if (gadgets[index].vaddr == addr) {
-          scrollTableToSelectedRow(jqt, index);
-          break;
-      }
+    var index = findGadgetIndexForVaddr(gadgets, vaddr);
+    if (index >= 0) {
+      scrollTableToSelectedRow(jqt, index);
     }
   });
 }
@@ -65,11 +142,41 @@ function scrollTableToSelectedRow(table, index) {
 
 
 function launchComparator() {
+  if ($(".ropTable").length > 1) {
+    $("#generate-surv-chain-btn").removeAttr("disabled");
+  } else {
+    $("#generate-surv-chain-btn").attr("disabled", "disabled");
+  }
   $("#comparator-root").unblock();
+
 }
 
 function toHash(gadgets) {
   for (gi in gadgets) {
     var gadget = gadgets[gi];
   }
+}
+
+function findGadgetIndexForVaddr(gadgets, vaddr) {
+  for (var index in gadgets) {
+    if (gadgets[index].vaddr == vaddr) {
+      return index;
+    }
+  }
+  return -1;
+}
+
+function findGadgetIndexForGadget(gadgets, gadget) {
+  for (var index in gadgets) {
+    if (gadgets[index].gadget == gadget) {
+      return index;
+    }
+  }
+  return -1;
+}
+
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive
 }
